d99c3f911ce942294e117bd572bcea9df1416e49
BorryS <andrzej.kimel@gmail.com>
2016-04-02 11:30:45 +0200

    Some fixes to continuous deployment configuration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -jar target/pet-vet-full.jar
--- src/main/java/com/github/monikaemm/Main.java
+++ src/main/java/com/github/monikaemm/Main.java
@@ -1,14 +1,17 @@
 package com.github.monikaemm;
+import java.util.Optional;
+
 import static spark.Spark.*;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Monika Hołysz on 02.04.2016.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        int portNumber = Optional.ofNullable(System.getProperty("server.port"))
+                .map(Integer::parseInt)
+                .orElse(4567);
+        port(portNumber);
 
         get("/hello", (req,res) -> "Hello World");
     }
