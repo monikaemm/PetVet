b5403dbdfe8267e20abe91f6e07e64ffe319f213
Monika <monika.miluch@gmail.com>
2016-04-02 15:37:24 +0200

    Removed unused code.

--- src/main/java/com/github/monikaemm/Main.java
+++ src/main/java/com/github/monikaemm/Main.java
@@ -33,47 +33,17 @@ public class Main {
             return new ModelAndView(helloModel, "hello");
         }, templateEngine);
 
-        Visit visit1 = new Visit();
-        visit1.setDate(LocalDateTime.of(2016, 4, 10, 15, 30));
-        visit1.setName("Surname1");
-        visit1.setSpecies("dog");
-        visit1.setPurpose("recognition");
-
-        Visit visit2 = new Visit();
-        visit2.setDate(LocalDateTime.of(2016, 4, 9, 10, 30));
-        visit2.setName("Surname2");
-        visit2.setSpecies("cat");
-        visit2.setPurpose("recognition");
-
-        Visit visit3 = new Visit();
-        visit3.setDate(LocalDateTime.of(2016, 4, 13, 14, 0));
-        visit3.setName("Surname1");
-        visit3.setSpecies("dog");
-        visit3.setPurpose("recognition");
-
-
-        List<Visit> visitList = new ArrayList<>();
-        visitList.add(visit1);
-        visitList.add(visit2);
-        visitList.add(visit3);
-        visitList.sort((v1, v2) -> v1.getDate().compareTo(v2.getDate()));
 
         get("/registering_visit", (req, res) -> {
             Map<String, Object> visitModel = new HashMap<>();
-            visitModel.put("registering_visits", visit1);
             return new ModelAndView(visitModel, "registering_visit");
         }, templateEngine);
         post("/registering_visit", (req, res) -> {
-            String ownerName = req.queryParams("name");
-            String speciesName = req.queryParams("species");
-            String purposeName = req.queryParams("purpose");
-            String dateName = req.queryParams("date");
             Visit visit = new Visit();
-            visit.setName(ownerName);
-            visit.setSpecies(speciesName);
-            visit.setPurpose(purposeName);
-            visit.setDate(LocalDateTime.parse(dateName, Visit.formatter));
-            visitList.add(visit);
+            visit.setName(req.queryParams("name"));
+            visit.setSpecies(req.queryParams("species"));
+            visit.setPurpose(req.queryParams("purpose"));
+            visit.setDate(LocalDateTime.parse(req.queryParams("date"), Visit.formatter));
 
             saveToDb(visit);
 
